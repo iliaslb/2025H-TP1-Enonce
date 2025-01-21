c3019e7ae4789fda05f06ed4357c483959acce0b
Ilias El Benani <52327251+iliaslb@users.noreply.github.com>
2025-01-21 16:43:13 -0500

    Exo 1

--- exo1.py
+++ exo1.py
@@ -6,3 +6,10 @@
 
 # Afficher un message de bienvenue avec le nom complet
 # Afficher l'année de naissance
+name = input("Veuillez entrer votre nom complet :")
+age = int(input("Veuillez entrer votre âge :"))
+annee = 2025
+naissance = annee - age
+
+print('Bonjour', name)
+print('Vous êtes né(e) en', naissance)
