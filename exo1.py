51dbaa170c4a94dcc2d4deea8c7d2ac61b765029
Ricardo Acuna <ricardo-jose.acuna-de-aguas@polymtl.ca>
2025-01-21 17:02:20 -0500

    question 1 et 2

--- exo1.py
+++ exo1.py
@@ -1,15 +1,12 @@
 # Demander le nom complet de l'utilisateur
+nom = input("Veuillez entrer votre nom complet : ")
 # Demander l'âge de l'utilisateur
-
+age = int(input("Veuillez entrer votre âge : "))
 # Définir l'année actuelle
+ANACT = 2025
 # Calculer l'année de naissance
-
+anNai = ANACT - age
 # Afficher un message de bienvenue avec le nom complet
+print('Bonjour', nom)
 # Afficher l'année de naissance
-name = input("Veuillez entrer votre nom complet :")
-age = int(input("Veuillez entrer votre âge :"))
-annee = 2025
-naissance = annee - age
-
-print('Bonjour', name)
-print('Vous êtes né(e) en', naissance)
+print('Vous êtes né(e) en', anNai)
--- exo2.py
+++ exo2.py
@@ -1,13 +1,18 @@
 # Demandez à l'utilisateur d'entrer le niveau de charge actuel de la batterie
-
+nivBat = int(input("Entrez le niveau de charge actuel de la batterie : "))
 # Vérifiez si le niveau de charge est valide
-
+if nivBat in range(0, 101):
+    nivBat = nivBat
+else:
+    print('Erreur : niveau de charge invalide.')
+    exit()
 # Arrondir le pourcentage à la dizaine la plus proche
-
+nivBatRd = round(nivBat, -1)
 # Calculer le nombre de barres
-
+nb =  nivBatRd / 10
 # Afficher la représentation graphique de la charge de la batterie
-
+print('[' + '❚' * int(nb) + ' '* (10 - int(nb)) + ']')
+print (str(nivBat) +'%')
 # Exemple d'utilisation :
 # Si l'utilisateur entre 76, la sortie sera :
 # [❚❚❚❚❚❚❚❚     ]
