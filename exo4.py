a5d8db6377b0dc3108d151a52ff8c6fa327aa416
Ilias El Benani <52327251+iliaslb@users.noreply.github.com>
2025-01-22 16:59:53 -0500

    exo 4 et 5

--- exo4.py
+++ exo4.py
@@ -4,16 +4,23 @@ secondes = int(input("Entrez un nombre de secondes : "))
 annees = semaines = jours = heures = minutes = 0
 
 # TODO: Assigner à la variable "annees" le nombre d'années
-
+annees = secondes // 31536000
+secondes -= annees*31536000
 # TODO: Assigner à la variable "semaines" le nombre de semaines restantes
-
+semaines = secondes // 604800
+secondes -= semaines*604800
 # TODO: Assigner à la variable "jours" le nombre de jours restants
-
+jours = secondes // 86400
+secondes -= jours*86400
 # TODO: Assigner à la variable "heures" le nombre d'heures restantes
-
+heures = secondes // 3600
+secondes -= heures*3600
 # TODO: Assigner à la variable "minutes" le nombre de minutes restantes
+minutes = secondes // 60
 
 # TODO: Assigner à la variable "secondes" le nombre de secondes restantes
+secondes -= minutes*60
 
 # TODO: Afficher le nombres d'années, semaines, jours, heures, minutes et secondes
-# Exemple: print(annees, semaines, jours, heures, minutes, secondes)
+print(annees, semaines, jours, heures, minutes, secondes)
+
--- exo5.py
+++ exo5.py
@@ -5,15 +5,16 @@ g = 9.8
 
 # Demander la vitesse initiale de la boule
 # Demander la vitesse initiale de la boule
-
+v = float(input("Vitesse initiale (en m/s): "))
 # Demander l'angle de lancement
-
+angle = float(input("Angle de lancer (en degrés): "))
 # Convertir l'angle en radians
+angle = (angle*math.pi)/180
 
 # Calculer la distance maximale en x
-
+x = ((v**2)*math.sin(2*angle))/g
 # Afficher la distance maximale arrondie à 2 chiffres après la virgule
-
+print("Distance parcourue: {:.2f}m".format(x))
 # Exemple:
 # Pour une vitesse initiale de 10 m/s et un angle de 45 degrés
 # La distance parcourue serait de 10.20m
